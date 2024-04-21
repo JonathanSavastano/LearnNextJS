551ea2496780354200e914f2f5f48149097ac775
Jonny Savastano <jonathan.savastano1246@gmail.com>
2024-04-21 14:16:43 -0600

    added streaming to page.tsx

---
+++ app/Dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from "../../ui/skeletons"; // imported in chapter 9
+
+export default function Loading() {
+    return <DashboardSkeleton />;
+}
--- app/Dashboard/loading.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardSkeleton from "../ui/skeletons"; // imported in chapter 9
-
-export default function Loading() {
-    return <DashboardSkeleton />;
-}
